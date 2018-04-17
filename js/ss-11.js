d20ee12b3d8db58df0bbf15b6f4c848ddddbde16
Scott <stsmith@callutheran.edu>
2018-04-17 13:38:16 -0700

    added comment at top

--- js/ss-11.js
+++ js/ss-11.js
@@ -1,3 +1,5 @@
+// JS for smithscott.net
+
 var $d = document;
 var p_items = [];
 var p_elems = $d.querySelectorAll('.portfolio-item');
@@ -16,7 +18,7 @@ var scroll_nav = {
 
 var lazyLoadImg = function(imgs){
 	for (i = 0; i < imgs.length; ++i) {
-		imgs[i].srcset = imgs[i].dataset.src;	
+		imgs[i].srcset = imgs[i].dataset.src;
 	}
 }
 
@@ -25,10 +27,10 @@ function isInViewport(element) {
 	const rect = element.getBoundingClientRect();
 	const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
 	const windowWidth = (window.innerWidth || document.documentElement.clientWidth);
-	
+
 	const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
 	const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0);
-	
+
 	return (vertInView && horInView);
 }
 
@@ -38,17 +40,17 @@ function isInViewport(element) {
 
 
 window.onload = function(e){
-	
-	// generate list of portfolio items	
+
+	// generate list of portfolio items
 	for(i = 0; i < p_elems.length; ++i) {
 		p_items.push({
 			'id': p_elems[i].id,
 			'loaded': false
 		});
-		
+
 		//console.log(isInViewport($d.querySelector('#'+p_elems[i].id)));
 	}
-		
+
 }
 
 
@@ -56,24 +58,21 @@ window.onload = function(e){
 
 
 window.onscroll = function(e){
-	
+
 	// check scroll position against portfolio list
-	for(pi in p_items){	
+	for(pi in p_items){
 		//scroll_nav.off(p_items[pi]);
-		
+
 		if(isInViewport($d.querySelector('#'+p_items[pi].id))){
 			//scroll_nav.on(p_items[pi]);
-						
+
 			// setup lazy load images
-			if(p_items[pi].loaded == false){			
+			if(p_items[pi].loaded == false){
 				var imgs = $d.querySelectorAll('#'+p_items[pi].id+' img');
 				lazyLoadImg(imgs);
 				p_items[pi].loaded = true;
 			}
-		} 
+		}
 	}
-	
+
 }
-
-
-
